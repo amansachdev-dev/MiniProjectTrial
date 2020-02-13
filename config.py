184bea6ea33330ac2fa7b0eebe5b5c84131b24fe
amansachdev-dev <52756369+amansachdev-dev@users.noreply.github.com>
2020-02-13 14:05:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TOKEN = '829018748:AAHnZnTx9gHDKaVn7d30nZ0vso5Wpnx8ESI'
-NGROK_URL = 'https://7a1e29dd.ngrok.io'
+TOKEN = ''
+NGROK_URL = ''
 BASE_TELEGRAM_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
 LOCAL_WEBHOOK_ENDPOINT = '{}/webhook'.format(NGROK_URL)
 TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, LOCAL_WEBHOOK_ENDPOINT)
